eb7db102036913fd07147ab2dae24dc7be414b76
Красильников Павел <krasilnikovpv@it2g.ru>
2024-10-08 01:53:58 +0300

    [feature] make post end point
    Наполнил DTO необходимыми полями

--- src/main/java/ru/paysonix/test/test_app/web/dto/SignatureProcessResponseDTO.java
+++ src/main/java/ru/paysonix/test/test_app/web/dto/SignatureProcessResponseDTO.java
@@ -1,4 +1,18 @@
 package ru.paysonix.test.test_app.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignatureProcessResponseDTO {
+    private String status;
+
+    public List<SignatureResponseDTO> result;
 }
--- src/main/java/ru/paysonix/test/test_app/web/dto/SignatureResponseDTO.java
+++ src/main/java/ru/paysonix/test/test_app/web/dto/SignatureResponseDTO.java
@@ -1,14 +1,14 @@
 package ru.paysonix.test.test_app.web.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class SignatureProcessRequestDTO {
-    private String formFieldName;
-
-    private String formFieldValue;
+public class SignatureResponseDTO {
+    private String signature;
 }
