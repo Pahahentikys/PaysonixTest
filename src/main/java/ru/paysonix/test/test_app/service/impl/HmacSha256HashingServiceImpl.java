f6c26bfcbd40e2b2830724b77001417218c320e4
Красильников Павел <krasilnikovpv@it2g.ru>
2024-10-08 18:53:33 +0300

    [feature] make post end point
    Для HmacSha256HashingServiceImpl добавил аннотацию @Service, внутри сервиса добавил реализацию метода: "makeHash"

--- src/main/java/ru/paysonix/test/test_app/service/impl/HmacSha256HashingServiceImpl.java
+++ src/main/java/ru/paysonix/test/test_app/service/impl/HmacSha256HashingServiceImpl.java
@@ -1,12 +1,42 @@
 package ru.paysonix.test.test_app.service.impl;
 
 import lombok.NonNull;
+import org.springframework.stereotype.Service;
 import ru.paysonix.test.test_app.service.HashingService;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@Service
 public class HmacSha256HashingServiceImpl implements HashingService {
+
+    private static final String HMAC_SHA_256 = "HmacSHA256";
+    private static final String HASHING_KEY = "hmackKey";
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
 
     @Override
     public @NonNull String makeHash(@NonNull String inputString) {
-        return "";
+        SecretKeySpec secretKeySpec = new SecretKeySpec(HASHING_KEY.getBytes(), HMAC_SHA_256);
+        try {
+            Mac mac = Mac.getInstance(HMAC_SHA_256);
+            mac.init(secretKeySpec);
+            return bytesToHex(mac.doFinal(inputString.getBytes()));
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
