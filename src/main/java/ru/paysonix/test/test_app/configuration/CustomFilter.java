6142aa91506ce907e2e645baeb08db84661f7390
Красильников Павел <krasilnikovpv@it2g.ru>
2024-10-09 21:36:07 +0300

    [feature] spring security
    В CustomFilter добавил проверку входящего кастомного заголовка на null

--- src/main/java/ru/paysonix/test/test_app/configuration/CustomFilter.java
+++ src/main/java/ru/paysonix/test/test_app/configuration/CustomFilter.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
+import static java.util.Objects.isNull;
+
 @RequiredArgsConstructor
 public class CustomFilter extends OncePerRequestFilter {
     private static final String CUSTOM_TOKEN_HEADER = "Token";
@@ -20,6 +22,11 @@ public class CustomFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var reqHeader = request.getHeader(CUSTOM_TOKEN_HEADER);
+
+        if (isNull(reqHeader)) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
 
         var decodedTokenFromHeader = new String(Base64.getDecoder().decode(reqHeader.getBytes(StandardCharsets.UTF_8)));
 
