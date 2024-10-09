d6464bc405d7cbb8c8a9d675d50aae1f6977752b
Красильников Павел <krasilnikovpv@it2g.ru>
2024-10-09 22:36:34 +0300

    [feature] refactoring second part
    Выделил интерфейс для веб-сервиса

--- src/main/java/ru/paysonix/test/test_app/web/service/WebSignatureProcessingService.java
+++ src/main/java/ru/paysonix/test/test_app/web/service/WebSignatureProcessingService.java
@@ -1,52 +1,9 @@
 package ru.paysonix.test.test_app.web.service;
 
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import ru.paysonix.test.test_app.service.HashingService;
 import ru.paysonix.test.test_app.web.dto.FormFiledDTO;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static java.util.Comparator.comparing;
-
-@Service
-@RequiredArgsConstructor
-public class WebSignatureProcessingService {
-    private final HashingService hashingService;
-
-    @Value("${test-app.header.token}")
-    private String storedHeaderToken;
-
-    @Value("${test-app.form.secret.key}")
-    private String storedFormSecretKey;
-
-
-    public boolean hasValidTokenInHeader(@NonNull String tokenHeader) {
-        var decodedTokenFromHeader = new String(Base64.getDecoder().decode(tokenHeader.getBytes(StandardCharsets.UTF_8)));
-
-        return decodedTokenFromHeader.equals(storedHeaderToken);
-    }
-
-    public String processSignatureEncodedAsBase64(List<FormFiledDTO> form) {
-        return encodeAsBase64(hashingService.makeHash(makeFormattedString(form), storedFormSecretKey));
-    }
-
-    private String encodeAsBase64(String signature) {
-        return Base64.getEncoder().encodeToString(signature.getBytes(StandardCharsets.UTF_8));
-    }
-
-    private String makeFormattedString(List<FormFiledDTO> formFields) {
-        var sortedFormParams = formFields.stream()
-                .sorted(comparing(FormFiledDTO::getFormFieldName))
-                .toList();
-
-        return sortedFormParams.stream()
-                .map(ff -> "%s=%s".formatted(ff.getFormFieldName(), ff.getFormFieldValue()))
-                .collect(Collectors.joining("&"));
-    }
+public interface WebSignatureProcessingService {
+    String processSignatureEncodedAsBase64(List<FormFiledDTO> form);
 }
---
+++ src/main/java/ru/paysonix/test/test_app/web/service/impl/WebSignatureProcessingServiceImpl.java
@@ -0,0 +1,40 @@
+package ru.paysonix.test.test_app.web.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import ru.paysonix.test.test_app.service.HashingService;
+import ru.paysonix.test.test_app.web.dto.FormFiledDTO;
+import ru.paysonix.test.test_app.web.service.WebSignatureProcessingService;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+
+@Service
+@RequiredArgsConstructor
+public class WebSignatureProcessingServiceImpl implements WebSignatureProcessingService {
+    private final HashingService hashingService;
+
+    @Value("${test-app.form.secret.key}")
+    private String storedFormSecretKey;
+
+    @Override
+    public String processSignatureEncodedAsBase64(List<FormFiledDTO> form) {
+        return encodeAsBase64(hashingService.makeHash(makeFormattedString(form), storedFormSecretKey));
+    }
+
+    private String encodeAsBase64(String signature) {
+        return Base64.getEncoder().encodeToString(signature.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String makeFormattedString(List<FormFiledDTO> formFields) {
+        return formFields.stream()
+                .sorted(comparing(FormFiledDTO::getFormFieldName))
+                .map(ff -> "%s=%s".formatted(ff.getFormFieldName(), ff.getFormFieldValue()))
+                .collect(Collectors.joining("&"));
+    }
+}
