2a00ffcb860425a4cc6a23c35c2e971403b79ca8
Красильников Павел <krasilnikovpv@it2g.ru>
2024-10-09 21:22:19 +0300

    [feature] spring security
    В TestAppApplication добавил exclude на UserDetailsServiceAutoConfiguration убрал автогенерирование пароля

--- src/main/java/ru/paysonix/test/test_app/TestAppApplication.java
+++ src/main/java/ru/paysonix/test/test_app/TestAppApplication.java
@@ -2,12 +2,13 @@ package ru.paysonix.test.test_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class TestAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestAppApplication.class, args);
+    }
 
 }
