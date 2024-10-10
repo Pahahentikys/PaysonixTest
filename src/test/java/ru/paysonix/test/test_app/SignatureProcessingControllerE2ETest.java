143c581a723c2b04a6b1373e1eddba7e3d64725b
Красильников Павел <krasilnikovpv@it2g.ru>
2024-10-10 20:24:10 +0300

    [feature] small-fixes
    Выделил интерфейс для веб-сервиса

--- src/test/java/ru/paysonix/test/test_app/SignatureProcessingControllerE2ETest.java
+++ src/test/java/ru/paysonix/test/test_app/SignatureProcessingControllerE2ETest.java
@@ -22,26 +22,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("E2E тест контроллера процессинга хэш-суммы")
 @RequiredArgsConstructor
 public class SignatureProcessingControllerE2ETest extends TestAppApplicationTests {
+    private static final String AUTH_HEADER_NAME = "Token";
 
     @Value("${test-app.header.token}")
     private String headerForTest;
 
     @Test
     @DisplayName("POST запрос на получения хэш-суммы формы")
-    void getAll() throws Exception {
-        var requestForm = new SignatureProcessRequestDTO(List.of(
-                new FormFiledDTO("testField1", "testValue2"),
-                new FormFiledDTO("testField2", "testValue2")));
+    void makeSignature200() throws Exception {
+        var requestForm = makeSignatureProcessRequestDTO();
 
         String expectedHmacSha256EncodedAsBase64 = "NTQwZjZiZTNiNGZiOTg3YTkzMjNkOGFlYTBiZDY5NjQyYmIxZDkyZDYwYzNhODliYzllNTY1MDQ0NGZmYjhiNg==";
 
         mockMvc.perform(post("/api/v1/signature/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(requestForm))
-                        .header("Token", Base64.getEncoder().encodeToString(headerForTest.getBytes(StandardCharsets.UTF_8))))
+                        .header(AUTH_HEADER_NAME, Base64.getEncoder().encodeToString(headerForTest.getBytes(StandardCharsets.UTF_8))))
                 .andExpect(jsonPath("$.status", is(SignatureProcessingState.SUCCESS.getName())))
                 .andExpect(jsonPath("$.result.[0].signature", is(expectedHmacSha256EncodedAsBase64)))
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("POST запрос на получения хэш-суммы формы с ошибкой авторизации")
+    void makeSignature403() throws Exception {
+        String wrongHeader = "wrongHeader";
+
+        mockMvc.perform(post("/api/v1/signature/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(makeSignatureProcessRequestDTO()))
+                        .header("Token", Base64.getEncoder().encodeToString(wrongHeader.getBytes(StandardCharsets.UTF_8))))
+                .andExpect(status().isForbidden())
+                .andDo(print());
+    }
+
+    private SignatureProcessRequestDTO makeSignatureProcessRequestDTO() {
+        return new SignatureProcessRequestDTO(List.of(
+                new FormFiledDTO("testField1", "testValue2"),
+                new FormFiledDTO("testField2", "testValue2")));
     }
 }
